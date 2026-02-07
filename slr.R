b361e7cc866de880002fa967899487af16e172d2
ManassehKemevor <kemevormanasseh@gmail.com>
2026-02-07 15:40:50 +0000

    i'm sure i'm done

--- slr.R
+++ slr.R
@@ -17,15 +17,15 @@ abline(regMod,col="blue",lwd=2)
 
 names(regMod)
  #normality of error terms
-hist(regMod$residuals)
-qqnorm(regMod$residuals,pch=19)
+hist(regMod$residuals,main = "Residuals")
+qqnorm(regMod$residuals,pch=19,main = "Residuals")
 qqline(regMod$residuals,lwd=2,col="#ff0058")
  
 #linearity assumption
-plot(x,regMod$residuals)
+plot(x,regMod$residuals,xlab ="years of experience",ylab ="residuals")
 abline(h=0)
  #constant variance assumption
-plot(regMod$fitted.values,regMod$residuals)
+plot(regMod$fitted.values,regMod$residuals,xlab = "predicted salary",ylab ="residuals")
 abline(h=0,col="blue",lwd=2)
 
 #we assume observation are independent 

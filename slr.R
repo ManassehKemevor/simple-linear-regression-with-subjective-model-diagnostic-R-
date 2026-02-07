9589194899dedf302cd48d2d5556253c470c8a2f
ManassehKemevor <kemevormanasseh@gmail.com>
2026-02-07 15:21:58 +0000

    almost done

--- slr.R
+++ slr.R
@@ -5,3 +5,32 @@ x<-data[["YearsExperience"]]
 y=data[["Salary"]]
 
 plot(x,y,pch=19,main = "scatter plot",xlab ="Years ofExperience",ylab="Salary" )
+ 
+cor.test(x,y)
+
+regMod=lm(y~x)
+summary(regMod)
+anova(regMod)
+confint(regMod)
+
+abline(regMod,col="blue",lwd=2)
+
+names(regMod)
+ #normality of error terms
+hist(regMod$residuals)
+qqnorm(regMod$residuals,pch=19)
+qqline(regMod$residuals,lwd=2,col="#ff0058")
+ 
+#linearity assumption
+plot(x,regMod$residuals)
+abline(h=0)
+ #constant variance assumption
+plot(regMod$fitted.values,regMod$residuals)
+abline(h=0,col="blue",lwd=2)
+
+#we assume observation are independent 
+
+newData=data.frame(x=7.6)
+a=predict(regMod,newdata =newData,interval ="prediction")
+a
+
